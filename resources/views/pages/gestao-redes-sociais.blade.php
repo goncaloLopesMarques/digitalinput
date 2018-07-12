93c4a50c0e4b7a7a77bcca3852248d27eb69bf2c
Unknown <goncalo.lopes.m@gmail.com>
2018-07-12 10:37:50 +0100

    começo da estrutura de paginas

---
+++ resources/views/pages/gestao-redes-sociais.blade.php
@@ -0,0 +1,53 @@
+@extends('master-page')
+
+
+
+@section('head')
+
+    <title>Gestão Redes Sociais Preços</title>
+    <meta name="description" content="">
+
+    <meta name="keywords" content="gestão de redes sociais,
+    agencia de redes sociais, gestão de redes sociais preço, preço gestão redes sociais">
+    @include('layout.head') 
+    
+@endsection
+
+
+
+@section('nav-bar')
+
+    @include('layout.nav-bar')
+    
+@endsection
+
+
+
+@section('main')
+
+    @include('layout.home-page')
+    
+@endsection
+
+
+
+@section('footer')
+
+    @include('layout.footer')
+
+@endsection
+
+
+
+@section('scripts-css')
+
+    @include('layout.scripts-css')
+    
+@endsection
+
+
+@section('modals')
+
+    @include('layout.modals')
+    
+@endsection
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -4,7 +4,17 @@
 
 @section('head')
 
-    @include('layout.head')
+    <title>Agência Marketing Digital | Empresa Web | Design Gráfico</title>
+    <meta name="description" content="Agência Marketing Digital em Leiria,
+    especializada em SEO, Webdesign e Design Gráfico. 
+    Conheça as vantagens de ser Parceiro!">
+
+    <meta name="keywords" content="agencia de comunicação,criar empresa,web marketing,
+    agencia de marketing digital,marketing digital 360,marketing digital empresas,
+    marketing digital portugal,micro e pequenas e médias empresas,estratégias de marketing digital,
+    agência digital; franquia marketing digital,marketing digital para empresas,agencias de mkt digital
+    serviços marketing digital,marketing digital para pequenas empresas">
+    @include('layout.head') 
     
 @endsection
 
