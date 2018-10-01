9fe89377ce387c7f5805dd6286f68143e4da3b7d
Unknown <miguelrodrigues@digitalinput.pt>
2018-10-01 13:32:06 +0100

    substituição do boostrap do mdb pelo da instalação do node-modules

--- resources/views/layout/head.blade.php
+++ resources/views/layout/head.blade.php
@@ -9,7 +9,7 @@
         <!-- Font Awesome -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
         <!-- Bootstrap core CSS -->
-        <link href="/css/bootstrap.css" rel="stylesheet">
+        {{-- <link href="/css/bootstrap.css" rel="stylesheet"> --}}
         <!-- Material Design Bootstrap -->
         <link href="/css/mdb.css" rel="stylesheet">
 
