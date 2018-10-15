68d9dc20e8d0d04e8a446fd0c4a0c181e6882e8f
Unknown <miguelrodrigues@digitalinput.pt>
2018-10-15 18:48:19 +0100

    titulos

--- resources/views/layout/consultoria-marketing-digital.blade.php
+++ resources/views/layout/consultoria-marketing-digital.blade.php
@@ -1,6 +1,7 @@
-<section class="mt-4 pages-header">
+<section class="white">
 
-	<h1>Consultoria Marketing Digital Empresas</h1>
+	<h1 class="section-title h1-responsive">Consultoria Marketing Digital</h1>
+	<h2 class="text-center h2-responsive">Micro e pequenas empresas</h2>
 
 </section>
 
@@ -10,12 +11,23 @@
 
 		<div class="row d-md-block">
 			
-			<h2>micro e pequenas empresas</h2>
-			<h2>Médias Empresas</h2>
-			<h2>PME's</h2>
-			<h2> Consultor marketing digital</h2>
-			<h2>consultoria marketing digital</h2>
-			<h2>especialista em marketing digital</h2>
+			<h3 class="h3-responsive">Médias Empresas, PME's, Consultor marketing digital, consultoria marketing digital, especialista em marketing digital</h3>
+
+
+
+			<p>Consultoria em Marketing Digital  - Tem um novo negócio em mente? Quer passar do papel para a acção? Precisa de criar um site? Se chegou a esta página, é porque sabe da importância que a Internet tem ou poderá ter no seu negócio. Se procura Consultoria em Marketing Digital e uma empresa parceira conte com os nossos serviços... Porquê?</p>
+
+ 
+
+			<p>Porque não construímos apenas um website, estudamos, optimizamos e adaptamos todo este processo à área de negócio da sua empresa.</p>
+
+ 
+
+			<p>Estudamos o ciclo de compra online do seu consumidor e criamos estratégias personalizadas para que consiga atrair novos clientes e ainda fidelizar e manter os actuais. Interligamos o seu website com ferramentas actuais e testadas, destinadas a criar empatia e retorno financeiro para a sua empresa.</p>
+
+ 
+
+			<p>As estratégias que desenvolvemos possibilitam a análise em tempo real das visitas ao seu website, fornecendo-lhe informações preciosas de como as suas páginas ou produtos estão a interagir com os seus utilizadores. Se procura Consultoria em Marketing Digital , não procure mais.... Contacte-nos!</p>
 
 		</div>
 
